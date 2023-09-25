5c4011905a70844dd08f3917e021b2b0bca38a53
ryu0210 <89006072+ryu0210@users.noreply.github.com>
2023-09-25 11:05:25 +0700

    Add files via upload

---
+++ Day1_Message.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Day1_Message() {
+    const name = "Ryufath" // If Empty string, it will not show the name, and return hello world instead
+    if (name){
+        return <h1>Hello {name}!</h1>;
+    }
+    return <h1>Hello World!</h1>;
+}
+
+export default Day1_Message
